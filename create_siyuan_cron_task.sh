cc666db5e0d6bc0fcbb714e981974a511d3aa471
root <root@localhost.localdomain>
2023-01-29 18:18:37 +0800

    add create crontask script

---
+++ create_siyuan_cron_task.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+path=/etc/crontab
+sudo echo "3,15 * * * * root `pwd`/sync_siyuan_data_p.sh" >> $path
+sudo echo "30 23 * * * root `pwd`/backup_siyuan_note_data_p.sh" >> $path
+sudo crontab /etc/crontab
