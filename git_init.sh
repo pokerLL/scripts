fd2df1c227fd2bd051b7d59d119de976ed0eb4e9
lei <wanglei311600@qq.com>
2023-05-07 12:44:15 +0800

    add git_init scripts

---
+++ git_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 检查参数
+if [ $# -ne 2 ]; then
+	  echo "Usage: $0 <username> <email>"
+	    exit 1
+fi
+
+# 获取参数
+username=$1
+email=$2
+
+# 检查email是否合法
+if [[ ! "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+	  echo "Error: Invalid email format"
+	    exit 1
+fi
+
+# 初始化git
+git config --global user.name "$username"
+git config --global user.email "$email"
