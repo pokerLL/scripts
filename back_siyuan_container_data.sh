9f98e374b5da30d4649a39540f8f9dc84995d150
wanglei <wanglei31160@qq.com>
2023-05-07 15:40:07 +0800

    back_siyuan_container_data.sh

---
+++ back_siyuan_container_data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then # 检查参数数量是否少于1个
+  echo "Usage: $0 container_name [container_path]"
+  exit 1
+fi
+
+container_name=$1
+
+if [ -z "$2" ]; then 
+  container_path="/home/docker/$container_name"
+else
+  container_path=$2
+fi
+
+source_folder_path="$container_path/data"
+
+if [ ! -d "$source_folder_path" ]; then # 检查源文件夹是否存在
+  echo "Error: $source_folder_path is not a valid folder"
+  exit 1
+fi
+
+docker stop $container_name
+
+sudo bash `pwd`/back_file_or_dir.sh $container_name $source_folder_path
+
+docker start $container_name
