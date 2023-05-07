c225161a002d258abeb30845867cd7590ea03bad
wanglei <wanglei31160@qq.com>
2023-05-07 15:40:27 +0800

    add_siyuan_cron_task.sh

---
+++ add_siyuan_cron_task.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Command and parameters
+command="sudo bash `pwd`/backup_siyuan_container_data.sh"
+params=("$@")
+
+# Add a cron job for each parameter
+for param in "${params[@]}"
+do
+    # Cron job
+    cron_job="3,15 * * * * root $command $param"
+
+    # Check if the cron job already exists
+    if sudo grep -Fxq "$cron_job" /etc/crontab; then
+        echo "Cron job already exists: $cron_job"
+    else
+        echo "Adding cron job: $cron_job"
+        echo "$cron_job" | sudo tee -a /etc/crontab > /dev/null
+    fi
+done
+
+# Update crontab
+sudo crontab /etc/crontab
