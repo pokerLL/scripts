ff70203bba6912317abca2be14b5fbeb6111ad2b
wanglei <leiplus1024@gmail.com>
2023-05-10 16:45:28 +0800

    add:auto back data in /home/docker

---
+++ back_docker_service_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASE_DIR="$(readlink -f $(dirname "$0"))"
+DOCKER_HOME=/home/docker
+cd $DOCKER_HOME
+
+for dir in */; do
+  # 判断是否存在docker-compose.yml文件
+  if [ ! -f "$dir/docker-compose.yml" ]; then
+    echo "No docker-compose.yml file found in directory $dir, skipping..."
+    continue
+  fi
+
+  cd "$dir"
+
+  docker-compose down
+  cd ..
+done
+
+bash "$BASE_DIR"/back_file_or_dir.sh /mnt/DATA/docker $DOCKER_HOME
+bash "$BASE_DIR"/start_docker_service.sh
