009799ba715f878e7411b1466ca81a352382a5d4
lei <llggg2323@163.com>
2023-07-05 04:17:47 +0800

    update

--- _init.sh
+++ _init.sh
@@ -23,7 +23,7 @@ source /scripts-collection/proxy_tool.sh
 
 alias d='docker'
 alias dc='docker-compose'
-
+alias dcr='docker-compose down && docker-compose up -d'
 
 export PATH=$PATH:/scripts-collection
 
