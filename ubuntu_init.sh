33c66a091344de6af30e2c732bba048a47501191
lei <llggg2323@163.com>
2023-08-04 11:06:06 +0000

    add miniconda install script

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -9,19 +9,35 @@ packages=(
   "git"
   "vim"
   "openssh-server"
+  "rsync"
 )
 
 # 安装常用软件
 install_packages() {
+  sudo apt update
   for package in "${packages[@]}"; do
     echo "正在安装软件包：$package"
     sudo apt install -y "$package"
   done
 }
 
+# 安装conda
+install_miniconda(){
+   set -e
+   wget "https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O ~/miniconda.sh
+   bash ~/miniconda.sh -b -p $HOME/miniconda
+   ~/miniconda/bin/conda init
+   echo 'Successfully installed miniconda...'
+   echo -n 'Conda version: '
+   ~/miniconda/bin/conda --version
+   echo -e '\n'
+   exec bash
+}
+
 show_menu() {
   echo "===== 软件包安装菜单 ====="
-  echo "1. 安装软件包"
+  echo "1. 安装常用软件包"
+  echo "2. 安装Miniconda"
   echo "2. 退出"
   echo "========================"
 
@@ -30,9 +46,11 @@ show_menu() {
   case $choice in
     1)
       install_packages
-      echo "软件包安装完成"
       ;;
     2)
+      install_miniconda
+      ;;
+    3)
       echo "退出菜单"
       exit
       ;;
