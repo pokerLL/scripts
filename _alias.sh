a213993d90837c873059981659e75413b035ff87
lei <llggg2323@163.com>
2023-08-20 07:16:10 +0800

    update

--- _alias.sh
+++ _alias.sh
@@ -25,6 +25,10 @@ alias d='docker'
 alias dc='docker-compose'
 alias dcr='docker-compose down && docker-compose up -d'
 
+alias tree='tree -a'
+alias s='sudo systemctl'
+alias sr='sudo systemctl restart'
+alias sd='sudo systemctl stop'
 export PATH=$PATH:/scripts-collection
 
 
--- proxy_tool.sh
+++ proxy_tool.sh
@@ -1,7 +1,7 @@
 function proxy_on(){
-    export ALL_PROXY=socks5://127.0.0.1:7891
-    export http_proxy=http://127.0.0.1:7890
-    export https_proxy=https://127.0.0.1:7890
+    export ALL_PROXY=socks5://127.0.0.1:20170
+    export http_proxy=http://127.0.0.1:20171
+    export https_proxy=http://127.0.0.1:20171
     echo -e "Enabled proxy..."
 }
 
