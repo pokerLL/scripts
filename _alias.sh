3765c77e0c7a8221226633642b01afd1cd256333
lei <llggg2323@163.com>
2023-08-28 12:03:29 -0400

    add miniconda.sh

---
+++ _alias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+alias ll='ls -alh'
+alias w='watch'
+
+alias d='docker'
+alias dc='docker-compose'
+alias dcr='docker-compose down && docker-compose up -d'
+
+export PATH=$PATH:/scripts-collection
